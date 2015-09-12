402916e8936044f6ba8a90fb975332eced640053
lastcat <lastcat@kmc.gr.jp>
2015-09-12 05:58:49 +0000

    権限を777にした

--- webapp/ruby/unicorn_config.rb
+++ webapp/ruby/unicorn_config.rb
@@ -1,2 +1,14 @@
+app_path = "/home/isucon/webapp/ruby"
+
+#ソケット、pidファイル 設定
+#graceful restart(要はreloadは) sudo kill -s usr2 `cat unicorn.pid`で
+listen app_path + "/unicorn.sock"
+pid    app_path + "/unicorn.pid"
+
+#ログとか出力系設定
+log =  app_path + "/log/unicorn.log"
+
+
+
 worker_processes 10
 preload_app true
