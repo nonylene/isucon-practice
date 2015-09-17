41abe2f50b9e8305100a55d3e22aac3c7dc40aab
nonylene <cookie.speaker@gmail.com>
2015-09-17 16:14:13 +0000

    go get redis

--- webapp/go/build.sh
+++ webapp/go/build.sh
@@ -4,4 +4,5 @@ go get github.com/go-martini/martini
 go get github.com/go-sql-driver/mysql
 go get github.com/martini-contrib/render
 go get github.com/martini-contrib/sessions
+go get github.com/garyburd/redigo/redis
 go build -o golang-webapp .
