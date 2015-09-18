9b823ed724c0e69eb0ba10bfb4eb614c915322b3
nonylene <cookie.speaker@gmail.com>
2015-09-18 15:41:30 +0000

    add init sh

--- init.sh
+++ init.sh
@@ -7,7 +7,10 @@ myuser=root
 mydb=isu4_qualifier
 myhost=127.0.0.1
 myport=3306
+
 mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}; CREATE DATABASE ${mydb}"
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/schema.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_users.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
+
+/home/nonylene/isucon-practice/init
